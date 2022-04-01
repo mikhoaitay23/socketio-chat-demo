4365b2fb4d5c185691bfcd54d1efe18f179c20d6
unknown <thanhnv@eqtek.vn>
2022-04-01 17:57:38 +0700

    - update

---
+++ app/src/main/java/com/example/socketio_chat_demo/ui/splash/SplashFragment.kt
@@ -0,0 +1,18 @@
+package com.example.socketio_chat_demo.ui.splash
+
+import com.example.socketio_chat_demo.R
+import com.example.socketio_chat_demo.base.basefragment.BaseFragment
+import com.example.socketio_chat_demo.databinding.FragmentSplashBinding
+
+class SplashFragment : BaseFragment<FragmentSplashBinding>() {
+
+    override fun getLayoutID() = R.layout.fragment_splash
+
+    override fun initView() {
+
+    }
+
+    override fun initViewModel() {
+
+    }
+}
